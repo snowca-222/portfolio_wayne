0c559c3c4ed44bca01e4457108006b6ec80a392b
wayne <holybell222@gmail.com>
2022-02-11 16:12:29 +0800

    新增文件

---
+++ js/works_data.js
@@ -0,0 +1,344 @@
+const works_datas = [
+    {
+        "id": "ori1",
+        "listType": "graphic",
+        "name": "pose 練習",
+        "graphic": "ori1.jpg",
+        "content": "平時繪製的鉛筆稿。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "ori2",
+        "listType": "graphic",
+        "name": "擬人作畫練習",
+        "graphic": "ori2.jpg",
+        "content": "平時繪製的鉛筆稿。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "ori3",
+        "listType": "graphic",
+        "name": "角色創作練習",
+        "graphic": "ori3.jpg",
+        "content": "平時繪製的鉛筆稿。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "ori4",
+        "listType": "graphic",
+        "name": "電繪創作練習",
+        "graphic": "ori4.jpg",
+        "content": "平時繪製的電繪稿。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "rin1",
+        "listType": "graphic",
+        "name": "臨摹練習",
+        "graphic": "rin1.jpg",
+        "content": "國中時期繪製的臨摹作品 - 實況野球君。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "rin2",
+        "listType": "graphic",
+        "name": "臨摹練習",
+        "graphic": "rin2.jpg",
+        "content": "國中時期繪製的臨摹作品 - 幸運女神 / 洛克人X-AXL。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "su1",
+        "listType": "graphic",
+        "name": "素描練習",
+        "graphic": "su1.jpg",
+        "content": "國中時期繪製的部分僅存素描練習。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "2d1",
+        "listType": "graphic",
+        "name": "灰階上色練習",
+        "graphic": "2D_1.jpg",
+        "content": "利用灰階上色的概念進行練習，參考角色為「來自深淵 - 娜娜奇」。",
+        "movie_url": "vSvzLOteTIA",
+        "web": ""
+    },
+    {
+        "id": "2d2",
+        "listType": "graphic",
+        "name": "場景電繪 - 劍塚",
+        "graphic": "2D_2.jpg",
+        "content": "以劍塚為主題所繪製的背景作品，受同事委託進行的繪製，劍的造型參考了相當多與劍相關的資料後才進行設計與繪製。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "2d3",
+        "listType": "graphic",
+        "name": "場景電繪 - 天空雲層",
+        "graphic": "2D_3.jpg",
+        "content": "當年使用在「手遊 - 音速少女隊」形象網頁背景繪製，",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "2d4",
+        "listType": "graphic",
+        "name": "節慶電繪 - 2016 萬聖節快樂",
+        "graphic": "2D_4.jpg",
+        "content": "利用軟體 CSP 繪製後，在 PS 進行加工及調整，過程中參考了巫婆、靴子、服飾、房屋、雲等資料。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "2d5",
+        "listType": "graphic",
+        "name": "文字創作",
+        "graphic": "2D_5.jpg",
+        "content": "利用擠壓沐浴乳的方式寫出英文字母，在進行創作的海報及文字創造練習。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "3d1",
+        "listType": "graphic",
+        "name": "場景製作 - 森林",
+        "graphic": "3D_1.jpg",
+        "content": "使用 Blender 3D 所創作的 low poly 森林小徑場景。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "3d2",
+        "listType": "graphic",
+        "name": "角色製作 - King KAZMA",
+        "graphic": "3D_2.jpg",
+        "content": "使用 Zbrush 所創作的角色，參考自「夏日大作戰 - King KAZMA」。此次製作先是從高模進行雕塑後再重新進行 topology，有部分元素加入了一點自己想表現的設計方式，讓作品看起來更帥氣。",
+        "movie_url": "wQyRn8ps05E",
+        "web": ""
+    },
+    {
+        "id": "3d3",
+        "listType": "graphic",
+        "name": "角色製作 - Rockman ZERO",
+        "graphic": "3D_3.jpg",
+        "content": "使用 MAYA 所創作的角色人物，該人物來自「洛克人X - ZERO」。製作當下利用貼圖做出金屬戰損掉漆的效果。",
+        "movie_url": "Slz_OvXyhAw",
+        "web": ""
+    },
+    {
+        "id": "3d6",
+        "listType": "graphic",
+        "name": "角色製作 - 射手女孩",
+        "graphic": "3D_6.jpg",
+        "content": "使用 ZBrush 進行創作及貼圖繪製，槍管造型是以鑰匙為概念進行創作。",
+        "movie_url": "OChJeIvFPIc",
+        "web": ""
+    },
+    {
+        "id": "3d7",
+        "listType": "graphic",
+        "name": "角色製作 - Belldandy 頭雕",
+        "graphic": "3D_7.jpg",
+        "content": "使用 ZBrush 進行創作的作品，角色參考自漫畫「幸運女神 - Belldandy」",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "3d9",
+        "listType": "graphic",
+        "name": "物件製作 - 磨豆機",
+        "graphic": "3D_9.png",
+        "content": "使用 Blender 並使用 cycle 進行渲染的磨豆機。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "3d10",
+        "listType": "graphic",
+        "name": "小木屋製作",
+        "graphic": "3D_10.jpg",
+        "content": "使用 Blender 3D 進行製作，用於自學 Blender 3D 渲染的練習作品。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "3d11",
+        "listType": "graphic",
+        "name": "馬場、馬匹、騎師製作過程",
+        "graphic": "3D_11.jpg",
+        "content": "替公司開發的 3D 賽馬動畫物件，全程使用 Blender 3D 進行製作。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "3d12",
+        "listType": "graphic",
+        "name": "遊戲按鈕 3D 轉 2D 製作",
+        "graphic": "3D_12.jpg",
+        "content": "替公司開發的按鈕按鈕，為了要有符合正確透視的設計，先利用 3D 製作模型後再利用向量軟體進行繪製。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "3d4",
+        "listType": "graphic",
+        "name": "Mental Ray 運算作品",
+        "graphic": "3D_4.jpg",
+        "content": "在 Maya 課堂中練習的作品，利用材質球及光源設定，透過 Mental Ray 運算結果。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "3d5",
+        "listType": "graphic",
+        "name": "3D 動畫 - 擲球",
+        "graphic": "3D_5.jpg",
+        "content": "參考上肩投球的 slow motion 動作後所創作的動畫作品。",
+        "movie_url": "ADjbuZyI1Q8",
+        "web": ""
+    },
+    {
+        "id": "3d13",
+        "listType": "graphic",
+        "name": "3D 動畫 - walking cycle 練習",
+        "graphic": "3D_13.jpg",
+        "content": "帶有俏皮行為的 walking cycle 動作練習。",
+        "movie_url": "j5KkT72COeA",
+        "web": ""
+    },
+    {
+        "id": "3d14",
+        "listType": "graphic",
+        "name": "3D 動畫 - 後空翻",
+        "graphic": "3D_14.jpg",
+        "content": "參考真實後空翻動畫後進行製作的動作練習。",
+        "movie_url": "KukOBJKpeAE",
+        "web": ""
+    },
+    {
+        "id": "3d15",
+        "listType": "graphic",
+        "name": "3D 建物 - 城堡",
+        "graphic": "3D_15.jpg",
+        "content": "過去針對建模進行練習的作品。",
+        "movie_url": "",
+        "web": ""
+    },
+    // {
+    //     "id": "3d16",
+    //     "listType": "graphic",
+    //     "name": "3D 動畫 - 最初的 3D 自製作品",
+    //     "graphic": "3D_16.jpg",
+    //     "content": "2013 年在學習 MAYA 時所製作的武士動畫，利用自己有限的技術去製作。當然以現在來看技術已提升，但還是將第一個自製動畫作品放來表示自己當初想踏入 3D 的原因。",
+    //     "movie_url": "wDPmiBwIqAA",
+    //     "web": ""
+    // },
+    {
+        "id": "3d8",
+        "listType": "graphic",
+        "name": "配件製作 - 靴子",
+        "graphic": "3D_8.png",
+        "content": "早期的 3D 靴子設計。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "wk1",
+        "listType": "works",
+        "name": "商業設計 - 北投有藝思",
+        "graphic": "works_1.jpg",
+        "content": "協助替合作夥伴所設計的北投有藝思活動提案設計稿。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "wk2",
+        "listType": "works",
+        "name": "商業設計 - 音速少女隊海報、掛軸、側邊攔廣告設計等",
+        "graphic": "works_2.jpg",
+        "content": "推廣【手遊 - 音速少女隊】時所設計的周邊商品及宣傳 PV 影片。其中在 PV 影片的腳本分鏡、製作等皆獨自發想，並與 PM 討論後製作成品。",
+        "movie_url": "-USRS4oy1k8",
+        "web": ""
+    },
+    {
+        "id": "wk3",
+        "listType": "works",
+        "name": "商業設計 - 音速少女隊遊戲推廣圖",
+        "graphic": "works_3.jpg",
+        "content": "推廣【手遊 - 音速少女隊】時所設計的廣告圖像及 LINE 貼圖設計。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "wk4",
+        "listType": "works",
+        "name": "商業設計 - 喵電時代 PV 影片",
+        "graphic": "works_4.jpg",
+        "content": "推廣【手遊 - 喵電時代】所設計的 PV 宣傳影片。其腳本分鏡、製作、畫面錄製皆獨自設計與處理。",
+        "movie_url": "OKL1kPX-yAc",
+        "web": ""
+    },
+    {
+        "id": "wk4",
+        "listType": "works",
+        "name": "商業設計 - 喵電時代 PV 影片 (國賓電子看板)",
+        "graphic": "works_5.jpg",
+        "content": "推廣【手遊 - 喵電時代】所設計的 PV 宣傳影片。並投放再台北西門國賓戶外電子看板上",
+        "movie_url": "4LPToX_3_tY",
+        "web": ""
+    },
+    {
+        "id": "wk6",
+        "listType": "works",
+        "name": "商業設計 - 遠征M PV 影片 (國賓電子看板)",
+        "graphic": "works_6.jpg",
+        "content": "替台灣淘米製作的 PV 影片，並投放再台北西門國賓戶外電子看板上。",
+        "movie_url": "eewf-hQjVq0",
+        "web": ""
+    },
+    {
+        "id": "w1",
+        "listType": "works",
+        "name": "網頁設計 - 好騎貸",
+        "graphic": "web_1.jpg",
+        "content": "與友人合作協助製作的網站，由我負責前台的設計與撰寫。過程中使用了Element UI 進行前後端分離處理，在這次製作過程中對 Vue 的撰寫也更加了解與掌握，也讓我開始去鑽研 Vue Cli 相關的資訊。",
+        "movie_url": "",
+        "web": "http://motor678.com/"
+    },
+    {
+        "id": "w2",
+        "listType": "works",
+        "name": "網頁設計 - 宏聚營造有限公司",
+        "graphic": "web_2.jpg",
+        "content": "是一個關於營造公司的形象網站，因此並沒有使用任何框架進行處理。資料擷取的部分使用 JSON 進行匯入。",
+        "movie_url": "",
+        "web": "http://www.hong-ju.com.tw/"
+    },
+    {
+        "id": "w3",
+        "listType": "works",
+        "name": "網頁設計 - 瑞啟會計師事務所",
+        "graphic": "web_3.jpg",
+        "content": "是一個關於會計師事務所的形象網站，由於內容不需要時常更新，因此在語系切換的部分是直接使用業主提供的翻譯文件進行處理。",
+        "movie_url": "",
+        "web": "https://www.richcpa.com.tw/"
+    },
+    {
+        "id": "w8",
+        "listType": "works",
+        "name": "網頁設計 - 深見企業有限公司",
+        "graphic": "web_8.jpg",
+        "content": "過去任職公司接洽的客戶專案，是一個含有購物車系統的網站。前端設計皆由我獨自發想，並與客戶來回討論後進行製作，購物車系統則交由配合的後端工程師協助處理。",
+        "movie_url": "",
+        "web": "https://www.oa-mart.com.tw/index.php"
+    }    
+]
